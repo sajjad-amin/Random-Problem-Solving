d8f5a366a6f94dc3ba654c12b509e0778f58defe
Sajjad Amin <sayem.1998.bd@gmail.com>
2021-12-09 13:53:04 +0600

    created facebook friend list downloading script

---
+++ fbFriendlistDownloader.js
@@ -0,0 +1,28 @@
+let friends = [];
+let frinedsDom = document.querySelectorAll('div[data-visualcompletion="ignore-dynamic"]');
+frinedsDom.forEach(e=>{
+    let name = e.querySelector('span').querySelector('span').innerText;
+    let link = e.querySelector('a') != null ? e.querySelector('a').href : '';
+    friends.push({name, link});
+})
+let data = '';
+let counter = 1;
+friends.forEach(e=>{
+    let singleData = `${counter}.\n\tName : ${e.name}\n\tLink : ${e.link}\n\n`;
+    data += singleData;
+    counter++;
+});
+let file = new Blob([data], {type: 'text/plain;charset=utf-8'});
+let str = document.documentElement.innerHTML.split('short_name')[1];
+let id_name = str.substr(str.indexOf('"')+3,str.indexOf('}')-4).toLowerCase();
+let filename = id_name+'_friendlist.txt';
+if (window.navigator.msSaveOrOpenBlob){
+    window.navigator.msSaveOrOpenBlob(file, filename);
+}
+else {
+    let a = document.createElement("a"),
+    url = URL.createObjectURL(file);
+    a.href = url;
+    a.download = filename;
+    a.click();
+}
